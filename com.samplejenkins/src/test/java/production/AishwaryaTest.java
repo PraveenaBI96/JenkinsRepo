2a66fbe1bc696a1f491631fd971cbce4fd199c87
Praveen <Praveen@LAPTOP-RQMIKUIO>
2022-07-19 15:56:26 +0530

    Push 1

---
+++ com.samplejenkins/src/test/java/production/AishwaryaTest.java
@@ -0,0 +1,33 @@
+package production;
+
+import org.testng.annotations.Test;
+
+public class AishwaryaTest {
+	@Test
+	public void Run()
+	{
+		System.out.println("i aded a run class in production pakage ");
+	}
+	@Test
+	public void Hai()
+	{
+		System.out.println("Hai");
+	}
+	@Test
+	public void Hello()
+	{
+		System.out.println("Hello");
+	}
+
+	public void RunTwo()
+	{
+		System.out.println("second run method");
+		System.out.println("Third changes");
+	}
+	@Test
+	public void RunTwo2()
+	{
+		System.out.println("second run method");
+		System.out.println("Third changes");
+	}
+}
