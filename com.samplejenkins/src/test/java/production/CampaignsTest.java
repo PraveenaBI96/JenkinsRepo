d55f7dd7dfd5ba583c1f2ae9d02141abaf504d11
Praveen <Praveen@LAPTOP-RQMIKUIO>
2022-07-19 17:31:02 +0530

    browser

--- com.samplejenkins/src/test/java/production/CampaignsTest.java
+++ com.samplejenkins/src/test/java/production/CampaignsTest.java
@@ -3,6 +3,16 @@ package production;
 import org.testng.annotations.Test;
 
 public class CampaignsTest {
+	
+	@Test
+	public void launchBrowser()
+	{
+		String browser = System.getProperty("Browser");
+		System.out.println(browser);
+
+		String url = System.getProperty("URL");
+		System.out.println(url);
+	}
 	@Test
 	public void createCampaign()
 	{
